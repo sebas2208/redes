46f9098448872107e1739cf91fde7458d0711085
sebas2208 <sebas2208@hotmail.com>
2022-06-06 15:35:14 -0500

    cambios

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,7 +8,7 @@ class About extends Component {
                     <h1>Sobre Mi</h1> 
 
                     <img src={Foto} width="180" height="220" alt="foto"/>
-                    <p>Mi nombre es Sebastian David Cubillos Medina, me encuentro cursando sexto semestre de Ingeniería de Sistema. Me gusta estar en un constante aprendizaje en donde 
+                    <p>Mi nombre es Sebastian David Cubillos Medina, me encuentro cursando sexto semestre de Ingeniería de Sistemas. Me gusta estar en un constante aprendizaje en donde 
                         desarrollo nuevas habilidades.
                     </p>
                 
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import contact from '../Components/public/image/contactlogo.png';
 import mailme from '../Components/public/image/mailmeimg.png';
 
 
@@ -7,10 +6,11 @@ class Contact extends Component {
     render() {
         return (
             <section className="container-1">
-
-                <img id="contactimg" src={contact} width="180" height="180" alt="contactlogo"/>
+                <svg width="180" height="180" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                </svg>
                 <h4>Correo: sebas2208c@gmail.com</h4>
-                <a id="mail"href="mailto:sebas2208c@gmail.com?Subject="><img id="mailmelogo" src={mailme} alt="mail me"/>Click Here To Send Mail</a>
+                <a id="mail"href="mailto:sebas2208c@gmail.com?Subject="><img id="mailmelogo" src={mailme} alt="mail me"/>Enviar correo electrónico</a>
                 <h4>Celular: 3165358003</h4>
 
                 
