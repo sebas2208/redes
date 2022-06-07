89bc217e4d08d211e65e8ebd7895036036942acc
sebas2208 <sebas2208@hotmail.com>
2022-06-06 21:21:57 -0500

    build

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import logo from '../Components/public/image/maxresdefault.jpg';
+import saludo from '../Components/public/image/saludo.png';
 
 class Banner extends Component {
     render() {
         return (
             <section className="container-banner">
 
-                    <a href="#home"><img id="profilepic" src={logo}  width="170" height="170" alt="profilepic"/></a>
+                    <a href="#home"><img id="profilepic" src={saludo}  width="200" height="200" alt="profilepic"/></a>
                     <h1>Hola, soy Sebastian </h1>
                     <p id="paragarph1"> Estudiante de Ingeniería de Sistemas</p> 
                     
